1413c2f04cd5983f0fc70849be6386c9de7a3787
Anthony Gonzalwez <84203906+SikawaNahi@users.noreply.github.com>
2021-05-15 01:32:32 +0545

    Best Badminton Rackets
    <a href="https://www.badmintonbuys.com/" rel="dofollow">best badminton rackets in the world</a> JS code.

---
+++ best badminton rackets.js
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Best Badminton Rackets List</h2>
+<p>Here are the top badminton rackets:
+<ol>
+  <li>1. Yonex Duora 10</li>
+  <li>2. Yonex Arcsaber 10</li>
+  <li>3. Yonex Arcsaber 11</li>
+<li>4. Yonex Voltric Z Force II</li>
+  <li>5. Yonex Nanoray Z Speed</li>
+  <li>6. Yonex Astrox 88D</li>
+<li>7. DSS Ares Red 68</li>
+  <li>8. Victor Brave Sword 12</li>
+  <li>9. Victor Jetspeed S 12</li>
+<li>10. Senston Woven HG Badminton Racket</li>
+  <li>11. Yonex 2 Nanoflare Drive Set</li>
+  <li>12. Li-Ning G Force Superlite 3600</li>
+<li>Yonex Nanoray 60FX</li>
+</ol>
+</p>
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML = 5 + 6;
+</script>
+
+</body>
+</html> 
